9bb4115ea3bbb6e957c9cb3f3f1175a881f41815
xliii <xliii@mail.com>
2022-03-19 11:20:29 +0200

    Basic data added

---
+++ TODO.md
@@ -0,0 +1,10 @@
+### TODO
+* landing page
+    * title
+    * list of mount offs
+    * socials
+* mount off page
+    * embed youtube
+    * description
+    * script/visualization
+* armory sync
--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-document.getElementById("container").innerHTML = "dynamic hello world";
+console.log("Initialize");
+
+let ul = document.getElementById("list");
+
+data.forEach(entry => {
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    let text = document.createTextNode(entry.date);
+    a.appendChild(text);
+    a.title = entry.date;
+    a.href = entry.url;
+
+    li.appendChild(a);
+
+    ul.appendChild(li);
+});
