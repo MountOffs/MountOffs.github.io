256dda35b410ed19f8ef43eb96f337390228bcfe
xliii <xliii@mail.com>
2022-03-25 20:55:51 +0200

    Background image for episodes

--- js/play.js
+++ js/play.js
@@ -3,8 +3,14 @@ let list = document.getElementById("episodes");
 episodes.filter(e => e.status === "done").forEach(episode => {
     let div = document.createElement("div");
     div.classList.add("gallery-cell");
+    setBackground(div, episode);
+
+    let backdrop = document.createElement("div");
+    backdrop.classList.add("backdrop");
+    div.appendChild(backdrop);
+
     let a = document.createElement("a");
-    let text = document.createTextNode(episode.date);
+    let text = document.createTextNode("Episode " + episode.id);
     a.appendChild(text);
     a.title = episode.date;
     a.href = "/mountoffs/episode.html?id=" + episode.id;
@@ -12,3 +18,8 @@ episodes.filter(e => e.status === "done").forEach(episode => {
     div.appendChild(a);
     list.appendChild(div);
 });
+
+function setBackground(div, episode) {
+    let imgUrl = "url('https://i.ytimg.com/vi/" + episode.youtubeId + "/hqdefault.jpg')";
+    div.style.background = imgUrl + " center";
+}
